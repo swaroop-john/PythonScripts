42573e20c17d8e514ed9a5b16216c9a9b82eaa40
swaroop-john <97107917+swaroop-john@users.noreply.github.com>
2022-04-01 10:42:46 +0100

    Add files via upload
    Program to list the index of an Excel column name. Example: A=1,B=2, Z=26,AA=27,AAA=703,BACD=35910...

---
+++ ColNameIndex.py
@@ -0,0 +1,25 @@
+#Python program to list the index number of an excel column name example: A=1,Z=26,AA=27,BA=53...
+#Dictionary with Alphabet key value pairs
+alphatable = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,'N':14,
+'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26}
+charlist=[] #Placeholder to convert the input string to a list
+#Function to derive the index value of a column name
+def indexvalue(colname,listlength):
+    k=1
+    power=0
+    sum=0
+    for i in charlist:
+        power=length-k
+        sum=sum+alphatable[i]*26**power
+        k=k+1
+    return(sum)
+
+#User input and validation
+colname=input("Please enter the column name: ")
+if colname.isalpha():
+    length=len(colname)
+    charlist=list(colname.upper())
+    colindex=indexvalue(charlist,length)
+    print("Index for",colname.upper(),"is:",colindex)
+else:
+    print("Invalid input! Please enter only letters (A - Z)")
